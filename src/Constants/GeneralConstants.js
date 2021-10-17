b824bf3d1caa9dde5cdf253d7695556611aea6a0
Ignacio Gonzalez <nachogonza123@gmail.com>
2021-10-17 18:46:36 -0300

    Cambios generales + Docker

---
+++ src/Constants/GeneralConstants.js
@@ -0,0 +1,12 @@
+const { join } = require('path');
+
+class GeneralConstants {
+    static DEFAULT_TIMEOUT = 60000;
+    static BASH_MSG_TIMEOUT = 15000;
+    static DEFAULT_COLOR = '#ff2929';
+    static DEFAULT_FOOTER = 'Develop by tato | afrt';
+
+    static DEFAULT_SERVER_FOLDER = join(__dirname, '../../ServidoresAFRT');
+}
+
+module.exports = { GeneralConstants };
