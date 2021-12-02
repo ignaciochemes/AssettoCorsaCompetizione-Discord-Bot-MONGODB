f5f42d280d109f1969353399e5905f30bd2056b6
Ignacio Gonzalez <nachogonza123@gmail.com>
2021-12-02 19:02:33 -0300

    DEV-11 Modifico constantes de las pistas

--- src/Constants/PistasConstants.js
+++ src/Constants/PistasConstants.js
@@ -1,24 +1,24 @@
 class Pistas {
     static pistas = [
-        "monza_2020",
-        "zolder_2020",
-        "brands_hatch_2020",
-        "silverstone_2020",
-        "paul_ricard_2020",
-        "misano_2020",
-        "spa_2020",
-        "nurburgring_2020",
-        "barcelona_2020",
-        "hungaroring_2020",
-        "zandvoort_2020",
-        "imola_2020",
-        "oulton_park_2019",
-        "donington_2019",
-        "snetterton_2019",
-        "kyalami_2019",
-        "mount_panorama_2019",
-        "suzuka_2019",
-        "laguna_seca_2019"
+        "monza",
+        "zolder",
+        "brands_hatch",
+        "silverstone",
+        "paul_ricard",
+        "misano",
+        "spa",
+        "nurburgring",
+        "barcelona",
+        "hungaroring",
+        "zandvoort",
+        "imola",
+        "oulton_park",
+        "donington",
+        "snetterton",
+        "kyalami",
+        "mount_panorama",
+        "suzuka",
+        "laguna_seca"
     ]
 }
 
